c808d5bd3b77604c8ad71fc2ef372f6e8e66076a
Mads <vpmads@gmail.com>
2018-09-17 14:07:38 +0200

    tilføjet klassen bil

---
+++ BilletLibrary/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilletLibrary
+{
+    class Car
+    {
+        /// <summary>
+        /// Klasse som indeholder vores propities Nummerplade og Dato samt to metoder til at returnere pris og køretøjs type. 
+        /// </summary>
+
+        public string Numberplate;
+        public DateTime Date;
+
+        public decimal price()
+        {
+            return 240;
+        }
+        public String Vehicle()
+        {
+            return "car"; 
+        }
+    }
+}
